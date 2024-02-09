73c94ba614352724c1c5ee7a19d9665f5788deaa
cavidanbagiri <cavidanbagiri@gmail.com>
2024-02-09 18:58:38 +0300

    Fix Create new product field

--- src/services/admin.service.js
+++ src/services/admin.service.js
@@ -6,22 +6,22 @@ const s3 = require('../storage/storage');
 
 class AdminServiceFetchCategoriesAndVariants {
 
-  static getCategoriesAndVariants = async  (req, res, next) => {
-    
+  static getCategoriesAndVariants = async (req, res, next) => {
+
     // Fetch All Categories For Alphabetic category_name
     const categories = await CategoryModels.findAll({
-      attributes:[
+      attributes: [
         "id", "category_name"
       ],
-      order:["category_name"]
+      order: ["category_name"]
     });
 
     // Fetch All Categories For Alphabetic variant_name
     const variants = await VariantsModels.findAll({
-      attributes:[
+      attributes: [
         "id", "variant_name"
       ],
-      order:["variant_name"]
+      order: ["variant_name"]
     });
 
     // Combine Results
@@ -29,82 +29,95 @@ class AdminServiceFetchCategoriesAndVariants {
       categories: [...categories],
       variants: [...variants]
     }
-    
+
     return return_data;
   }
 
 }
 
 class AdminServiceCreateProduct {
-  
+
   // Create Product
-  static async createProduct(product_data, product_variants, file){
+  static async createProduct(product_data, product_variants, file) {
 
-    // 0 - Upload Image
-    const uploading_images = await this.#uploadImage(file);
-    
-    // 1 - Create Product
-    const product = await this.#createProductInstance(product_data);
+    // If File is empty
+    if(file){
 
-    // 2 - Create Variant Value Model
-    await this.#createVariantValue(product, product_variants);
-
-    // 4 - Create product Image Instance
-    await this.#createProductImageInstance(uploading_images, product);
- 
-    return product;
-  }
-
-  //  Create Product Instance
-  static async #createProductInstance(product_data){
-    const product = await ProductModels.create({
-      categoryId: product_data.category_id,
-      product_name: product_data.product_name,
-      description: product_data.description,
-      sku:product_data.sku,
-      price:product_data.price,
-      stock:product_data.stock
-    });
-    return product;
-  }
-
-  // Save All Variants Value to VariantsValueModel
-  static async #createVariantValue(product, product_variants){
-    for(let i of product_variants){
-      // Create Variant Value
-      const variant_value = await VariantsValueModels.create({
-        variantsId: i.variant_id,
-        variant_value: i.value
-      }) 
-      // Insert product and variant value in the product_variants model
-      const product_variant = await ProductVariantModels.create({
-        productId: product.id,
-        variantsValueId: variant_value.id
-      })
+      // 0 - Upload Image
+      const uploading_images = await this.#uploadImage(file);
+  
+      // 1 - Create Product
+      const product = await this.#createProductInstance(product_data);
+  
+      // 2 - Create Variant Value Model
+      await this.#createVariantValue(product, product_variants);
+  
+      // 4 - Create product Image Instance
+      await this.#createProductImageInstance(uploading_images, product);
+      
+      return product;
     }
+    else{
+      // 1 - Create Product
+      const product = await this.#createProductInstance(product_data);
+      // 2 - Create Variant Value Model
+      await this.#createVariantValue(product, product_variants);
+    }
+
   }
 
   // Upload Image To Bucket and take location
-  static async #uploadImage(file){
+  static async #uploadImage(file) {
     const upload = await s3.Upload({
       buffer: file.buffer
     },
-    '/omarket_images'
+      '/omarket_images'
     )
     const res = await this.#createImageInstance(upload.Location);
     return res;
-  } 
+  }
 
   // Create instance Image to models
-  static async #createImageInstance(image_destination){
+  static async #createImageInstance(image_destination) {
     const res = await ImageModels.create({
       image_url: image_destination
     })
     return res;
   }
 
+  //  Create Product Instance
+  static async #createProductInstance(product_data) {
+    const product = await ProductModels.create({
+      categoryId: product_data.category_id,
+      product_name: product_data.product_name,
+      description: product_data.description,
+      sku: product_data.sku,
+      price: product_data.price,
+      stock: product_data.stock
+    });
+    return product;
+  }
+
+  // Save All Variants Value to VariantsValueModel
+  static async #createVariantValue(product, product_variants) {
+    for (let i of product_variants) {
+      if(i.variant_id !== ''){
+        // Create Variant Value
+        const variant_value = await VariantsValueModels.create({
+          variantsId: i.variant_id,
+          variant_value: i.value
+        })
+        // Insert product and variant value in the product_variants model
+        const product_variant = await ProductVariantModels.create({
+          productId: product.id,
+          variantsValueId: variant_value.id
+        })
+      }
+    }
+  }
+
   // Create ProductImage Instance
-  static async #createProductImageInstance(image, product){
+  static async #createProductImageInstance(image, product) {
     const res = await ProductImagesModels.create({
       imgId: image.id,
       productId: product.id
