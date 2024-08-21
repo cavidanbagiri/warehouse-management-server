631c17e7b3bf82d0e7017d9850cac4ab401af5c0
cavidanbagiri <cavidanbagiri@gmail.com>
2024-08-21 15:49:08 +0300

    Set new hostname for render

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ app.use(cookieParser());
 app.use(cors(
   {
     credentials: true,
-    origin: ['http://localhost:5173', 'http://localhost:4173'],
+    origin: ['http://localhost:5173', 'http://localhost:4173', 'https://warehouse-management-client.onrender.com'],
   }
 ));
 
